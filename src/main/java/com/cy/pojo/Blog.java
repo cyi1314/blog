e4b118b6e672bc8e08a1f5dfc1716a9d35bc0df2
cyi1314 <60090598+cyi1314@users.noreply.github.com>
2022-10-25 15:39:48 +0800

    Blog class

---
+++ src/main/java/com/cy/pojo/Blog.java
@@ -0,0 +1,49 @@
+package com.cy.pojo;
+
+public class Blog {
+    private Integer blog_id;
+    private Integer user_id;
+    private String blog_title;
+    private String blog_content;
+
+    public Blog(){}
+
+    public Blog(Integer blog_id,Integer user_id,String blog_title,String blog_content){
+        this.blog_id=blog_id;
+        this.user_id=user_id;
+        this.blog_title=blog_title;
+        this.blog_content=blog_content;
+    }
+
+    public Integer getBlog_id() {
+        return blog_id;
+    }
+
+    public void setBlog_id(Integer blog_id) {
+        this.blog_id = blog_id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getBlog_title() {
+        return blog_title;
+    }
+
+    public void setBlog_title(String blog_title) {
+        this.blog_title = blog_title;
+    }
+
+    public String getBlog_content() {
+        return blog_content;
+    }
+
+    public void setBlog_content(String blog_content) {
+        this.blog_content = blog_content;
+    }
+}
